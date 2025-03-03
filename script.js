8646181275b1ce605b7768910e22c57564382b77
Emilyempa <emilypettersson@hotmail.com>
2025-03-03 15:07:19 +0100

    commented out useless js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Simulerad blockering av tråden
-setInterval(() => {
-    let start = Date.now()
-    while (Date.now() - start < 1000) {}
-  }, 3000)
+// setInterval(() => {
+//     let start = Date.now()
+//     while (Date.now() - start < 1000) {}
+//   }, 3000)
   
   // Förhindrar användaren att navigera bakåt
   history.pushState(null, "", location.href)
@@ -14,13 +14,13 @@ setInterval(() => {
   }, 5000)
   
   // Loggar 10000 tal till konsolen efter 1 sekund
-  setTimeout(() => {
-    for (let i = 0; i < 10000; i++) {
-      console.log(i)
-    }
-  }, 1000)
+  // setTimeout(() => {
+  //   for (let i = 0; i < 10000; i++) {
+  //     console.log(i)
+  //   }
+  // }, 1000)
   
   // Loggar 100 000 siffror direkt när sidan laddas
-  for (let i = 0; i < 100000; i++) {
-    console.log(i)
-  }
+  // for (let i = 0; i < 100000; i++) {
+  //   console.log(i)
+  // }
